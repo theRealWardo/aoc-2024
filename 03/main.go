de53f046942b174f8ecf2c420d9dc28afc4d62c2
Matt Ward <contact@mwdesigns.com>
2024-12-02 23:12:28 -0800

    day 3 part 2

--- 03/main.go
+++ 03/main.go
@@ -9,14 +9,46 @@ import (
 )
 
 func parseOps(input string) [][]int {
-	r := regexp.MustCompile(`mul\([0-9]+\,[0-9]+\)`)
-	results := r.FindAllString(input, -1)
 	var out [][]int
-	for _, result := range results {
-		parts := strings.Split(result, ",")
-		num1, _ := strconv.Atoi(parts[0][4:])
-		num2, _ := strconv.Atoi(parts[1][:len(parts[1])-1])
-		out = append(out, []int{num1, num2})
+	enabled := true
+	prog := []string{}
+	for i := 0; i < len(input); {
+		cur := input[i:]
+		fmt.Printf("\ninput: %v\n", cur)
+		fmt.Printf("i: %v\n", i)
+		nextDisable := strings.Index(cur, "don't()")
+		nextEnable := strings.Index(cur, "do()")
+		fmt.Printf("nextDisable: %v\n", nextDisable)
+		fmt.Printf("nextEnable: %v\n", nextEnable)
+		if enabled {
+			if nextDisable == -1 || i+nextDisable > len(input) {
+				prog = append(prog, cur)
+				break
+			} else {
+				fmt.Printf("appending: %v\n", cur[:nextDisable])
+				prog = append(prog, cur[:nextDisable])
+				i = i + nextDisable + 7
+				enabled = false
+			}
+		} else {
+			if nextEnable == -1 {
+				break
+			} else {
+				i = i + nextEnable + 4
+				enabled = true
+			}
+		}
+	}
+	fmt.Printf("\n\n%+v\n\n", prog)
+	for i := 0; i < len(prog); i++ {
+		r := regexp.MustCompile(`mul\([0-9]+\,[0-9]+\)`)
+		results := r.FindAllString(prog[i], -1)
+		for _, result := range results {
+			parts := strings.Split(result, ",")
+			num1, _ := strconv.Atoi(parts[0][4:])
+			num2, _ := strconv.Atoi(parts[1][:len(parts[1])-1])
+			out = append(out, []int{num1, num2})
+		}
 	}
 	return out
 }
@@ -26,10 +58,12 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(parseOps(string(content)))
+	result := parseOps(string(content))
+
+	fmt.Println(result)
 
 	sum := 0
-	for _, op := range parseOps(string(content)) {
+	for _, op := range result {
 		sum += op[0] * op[1]
 	}
 	fmt.Println(sum)
